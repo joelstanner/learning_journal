137b174268a4bac7eed47e9bd7cd5c10811329c7
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-02-04 23:28:59 -0800

    added read entries tests

--- test_journal.py
+++ test_journal.py
@@ -2,9 +2,9 @@
 from contextlib import closing
 from pyramid import testing
 import pytest
+import datetime
 
-from journal import connect_db
-from journal import DB_SCHEMA
+from journal import INSERT_ENTRY, connect_db, DB_SCHEMA
 
 
 TEST_DSN = 'dbname=test_learning_journal user=Joel'
@@ -85,4 +85,32 @@ def test_write_entry(req_context):
     actual = rows[0]
     for idx, val in enumerate(expected):
         assert val == actual[idx]
+
+
+def test_read_entries_empty(req_context):
+    # call the function under test
+    from journal import read_entries
+    result = read_entries(req_context)
+    # make assertions about the result
+    assert 'entries' in result
+    assert len(result['entries']) == 0
+
+def test_read_entries(req_context):
+    # prepare data for testing
+    now = datetime.datetime.utcnow()
+    expected = ('Test Title', 'Test Text', now)
+    run_query(req_context.db, INSERT_ENTRY, expected, False)
+    # call the function under test
+    from journal import read_entries
+    result = read_entries(req_context)
+    # make assertions about the result
+    assert 'entries' in result
+    assert len(result['entries']) == 1
+    for entry in result['entries']:
+        assert expected[0] == entry['title']
+        assert expected[1] == entry['text']
+        for key in 'id', 'created':
+            assert key in entry
+
+
 
